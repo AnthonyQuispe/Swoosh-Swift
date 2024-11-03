d70bacf81f52dec252a7fe6160faa049f74aca90
Anthony <anthony.s.quispe@gmail.com>
2024-11-03 17:07:22 -0500

    Initial Commit

---
+++ SwooshTests/SwooshTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwooshTests.swift
+//  SwooshTests
+//
+//  Created by Anthony Quispe on 11/3/24.
+//
+
+import Testing
+@testable import Swoosh
+
+struct SwooshTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
